abe2a200d96195f6620c6dc01a4b9b6b81fafea3
hbestwork <gwzabota@gmail.com>
2026-04-24 17:22:25 +0300

    Final commit

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY', get_random_secret_key())
 
-CSRF_TRUSTED_ORIGINS = [env('APP_DOMAIN', 'localhost')]
+CSRF_TRUSTED_ORIGINS = [env('APP_DOMAIN', 'http://127.0.0.1')]
 
 DEBUG = env.bool('DEBUG', False)
 
